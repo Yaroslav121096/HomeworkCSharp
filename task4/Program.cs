aeabc9f3495406770d310394c6095c904d8cd0ee
Yaroslav <Valso96@mail.ru>
2022-11-27 04:26:56 +0600

    Нахождение максимального из трёх чисел

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (n > m && n > x)
+    Console.WriteLine($"Max = {n}");
+else if (m > n && m > x)
+    Console.WriteLine($"Max = {m}");
+else if (x > n && x > m)
+    Console.WriteLine($"Max = {x}");
+if (n == m && n == x)
+    Console.WriteLine($"Max = {n}");
+else if (m == n && m == x)
+    Console.WriteLine($"Max = {m}");
+else if (x == n && x == m)
+    Console.WriteLine($"Max = {x}");
