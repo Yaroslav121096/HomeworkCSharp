fac5bf76c055921c9bf62f89de5f48033fc009e4
Yaroslav <Valso96@mail.ru>
2022-11-27 04:39:25 +0600

    Нахождение третьей цифры числа через индекс

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(n);
+char[] m = str.ToCharArray();
+int i = 0;
+while (i < 2)
+    i++;
+if (n < 99)
+    Console.WriteLine("Третьей цифры нет!");
+else
+    Console.WriteLine(m[i]);
