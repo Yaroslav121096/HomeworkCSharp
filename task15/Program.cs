f0d8bfca327fb3fcbe842a501e931e3e83240c33
Yaroslav <Valso96@mail.ru>
2022-11-27 04:46:58 +0600

    Является ли заданный день недели выходным

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.WriteLine("Необходимо ввести цифру от 1 до 7: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n <= 5)
+    Console.WriteLine("Нет");
+if (n >= 6)
+    Console.WriteLine("Да");
