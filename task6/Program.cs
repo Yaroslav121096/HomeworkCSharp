cdbfb42d760d08b59f903339b0bf2029f9a132bb
Yaroslav <Valso96@mail.ru>
2022-11-27 04:30:31 +0600

    Чётное число или нечётное

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double m = n % 2;
+if (m == 0)
+    Console.WriteLine("Это число чётное");
+else 
+    Console.WriteLine("Это число нечётное");
