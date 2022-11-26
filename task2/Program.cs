5b81f8dc981aac032ccd330eeda8408a2782bad0
Yaroslav <Valso96@mail.ru>
2022-11-27 04:23:33 +0600

    Нахождение большего и меньшего из двух чисел

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine($"Max = {n}  Min = {m}");
+else
+    Console.WriteLine($"Max = {m}  Min = {n}");
