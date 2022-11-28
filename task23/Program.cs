1e732117ef154995958f9c77ecbb0ebf5a156d79
Yaroslav <Valso96@mail.ru>
2022-11-28 18:11:54 +0600

    Таблица кубов чисел

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Clear();
+Console.Write("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    if (i + 1 > n)
+        Console.Write($"{Math.Pow(i, 3)}.");
+    else
+        Console.Write($"{Math.Pow(i, 3)}, ");
+}
