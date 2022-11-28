0d166bf4f4d62fd202ed4d8f3c338e7a48e63565
Yaroslav <Valso96@mail.ru>
2022-11-28 18:08:58 +0600

    Является ли число палиндромом

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+int rev = 0;
+while (n > 0)
+{
+    rev = rev * 10 + n % 10;
+    n /= 10;
+}
+if (m == rev)
+    Console.WriteLine("Это число является палиндромом");
+else
+    Console.WriteLine("Это число не является
