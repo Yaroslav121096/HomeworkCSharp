0a4466e9263d6367549575402b6eff983cd11eb6
Yaroslav <Valso96@mail.ru>
2022-12-01 17:14:51 +0600

    Заполнение массива

---
+++ task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int[] array = new int[8];
+int i = 0;
+for(i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+string ar = string.Join(", ", array);
+Console.WriteLine($"[{ar}]");
