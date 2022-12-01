0c9be3b99a83b61995d65f231064a7ddf9f4ddcf
Yaroslav <Valso96@mail.ru>
2022-12-01 17:12:25 +0600

    Число a в степени b

---
+++ task 25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine($"Число {a} в степени {b} равно: {result}");
