11ef020e84bccb492f162f7f50220a445dc68df1
Yaroslav <Valso96@mail.ru>
2022-12-01 17:13:37 +0600

    Сумма цифр числа

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+    sum = sum + (n % 10);
+    n /= 10;
+}
+Console.WriteLine(sum);
